a6da81c803f55fb4e05702d086b671f8e4cb4b46
sk-ys <yskrmt@gmail.com>
2024-07-26 07:24:19 +0900

    Support for failed Issue ticket creation

--- lib/actual_dates/hooks.rb
+++ lib/actual_dates/hooks.rb
@@ -15,7 +15,7 @@ module ActualDates
       if params[:controller] == 'gantts'
         view_layouts_base_html_head_gantts(context)
       elsif params[:controller] == 'issues'
-        return unless ['show', 'new'].include? params[:action]
+        return unless ['show', 'new', 'create'].include? params[:action]
         context[:action] = params[:action]
         view_layouts_base_html_head_issues(context)
       end

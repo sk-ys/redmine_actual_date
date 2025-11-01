4b424751bf83a60ef39171f9ccb5e3ba34eee9cb
sk-ys <yskrmt@gmail.com>
2025-11-01 18:07:57 +0900

    Add form change detection to handle dynamic updates in issue view

--- assets/javascripts/rearrange_date_information.js
+++ assets/javascripts/rearrange_date_information.js
@@ -8,63 +8,65 @@ $(function () {
 
   // === issue attribute ===
   // --- configuration ---
-  const $targetIssueAttributes = $(
-    "#content div.issue.details div.attributes>div.splitcontent:first"
-  );
-  const $splitcontentDateinfoStr =
+  const targetIssueAttributesSelector =
+    "#content div.issue.details div.attributes>div.splitcontent:first";
+  const splitcontentDateinfoStr =
     "" +
     '<div class="splitcontent">' +
     '<div class="splitcontentleft"></div>' +
     '<div class="splitcontentleft"></div>' +
     "</div>";
 
-  const $splitcontent = $($splitcontentDateinfoStr);
-  const $splitcontentleftLeft = $splitcontent.children(
-    ".splitcontentleft:first"
-  );
-  const $splitcontentleftRight = $splitcontent.children(
-    ".splitcontentleft:last"
-  );
+  function moveElementsForView() {
+    const $targetIssueAttributes = $(targetIssueAttributesSelector);
+    const $splitcontent = $(splitcontentDateinfoStr);
+    const $splitcontentleftLeft = $splitcontent.children(
+      ".splitcontentleft:first"
+    );
+    const $splitcontentleftRight = $splitcontent.children(
+      ".splitcontentleft:last"
+    );
 
-  $targetIssueAttributes.after($splitcontent);
+    $targetIssueAttributes.after($splitcontent);
 
-  // --- move elements ---
-  // start date
-  $(
-    "#content div.issue.details div.attributes div.attribute.start-date:first"
-  ).appendTo($splitcontentleftLeft);
+    // --- move elements ---
+    // start date
+    $(
+      "#content div.issue.details div.attributes div.attribute.start-date:first"
+    ).appendTo($splitcontentleftLeft);
 
-  // due date
-  $(
-    "#content div.issue.details div.attributes div.attribute.due-date:first"
-  ).appendTo($splitcontentleftRight);
+    // due date
+    $(
+      "#content div.issue.details div.attributes div.attribute.due-date:first"
+    ).appendTo($splitcontentleftRight);
 
-  // actual start date
-  $(
-    "#content div.issue.details div.attributes div.attribute.cf_" +
-      actualStartDateCfId +
-      ":first"
-  ).appendTo($splitcontentleftLeft);
+    // actual start date
+    $(
+      "#content div.issue.details div.attributes div.attribute.cf_" +
+        actualStartDateCfId +
+        ":first"
+    ).appendTo($splitcontentleftLeft);
 
-  // actual end date
-  $(
-    "#content div.issue.details div.attributes div.attribute.cf_" +
-      actualEndDateCfId +
-      ":first"
-  ).appendTo($splitcontentleftRight);
+    // actual end date
+    $(
+      "#content div.issue.details div.attributes div.attribute.cf_" +
+        actualEndDateCfId +
+        ":first"
+    ).appendTo($splitcontentleftRight);
 
-  // estimated hours
-  // $('#content div.attributes div.attribute.estimated-hours:first')
-  //     .appendTo(splitcontentleft_left);
+    // estimated hours
+    // $('#content div.attributes div.attribute.estimated-hours:first')
+    //     .appendTo(splitcontentleft_left);
 
-  // add hr
-  var issue_attributes_content_last_text = $(
-    "#content div.issue.details div.attributes>div.splitcontent:last"
-  ).text();
+    // add hr
+    var issue_attributes_content_last_text = $(
+      "#content div.issue.details div.attributes>div.splitcontent:last"
+    ).text();
 
-  $splitcontent.before("<hr>");
-  if (issue_attributes_content_last_text !== "") {
-    $splitcontent.after("<hr>");
+    $splitcontent.before("<hr>");
+    if (issue_attributes_content_last_text !== "") {
+      $splitcontent.after("<hr>");
+    }
   }
 
   // === issue form ===
@@ -84,7 +86,7 @@ $(function () {
     " </div>" +
     "</fieldset>";
 
-  function moveElements() {
+  function moveElementsForEdit() {
     $("#issue-form fieldset.dateinfo").remove();
 
     // define fileldset and splitcontent
@@ -130,12 +132,48 @@ $(function () {
   }
 
   // initial invoke
-  moveElements();
+  moveElementsForView();
+  moveElementsForEdit();
 
   // override original function
   var replaceIssueFormWithOrg = replaceIssueFormWith;
   replaceIssueFormWith = function (html) {
     replaceIssueFormWithOrg(html);
-    moveElements();
+    moveElementsForEdit();
   };
+
+  /**
+   * Set up form change detection.
+   * This method's purpose is to detect when the issue view is replaced by
+   * other plugins like Redmine RT.
+   */
+  function setupFormChangeDetection() {
+    const targetNode = $("div.issue.details").parent()[0];
+
+    if (targetNode) {
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.type === "childList") {
+            mutation.addedNodes.forEach((node) => {
+              if (
+                node.nodeType === Node.ELEMENT_NODE &&
+                $(node).is("div.issue.details")
+              ) {
+                moveElementsForView();
+              }
+            });
+          }
+        });
+      });
+
+      observer.observe(targetNode, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+      });
+    }
+  }
+
+  // Initialize form change detection
+  setupFormChangeDetection();
 });
